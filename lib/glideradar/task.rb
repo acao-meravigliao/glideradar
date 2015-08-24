3ddd63d8141857a11d80a83d43ccd69319153dab
Daniele Orlandi <daniele@orlandi.com>
2015-08-25 01:46:39 +0200

    Implemented ObjectSpace dumping

--- lib/glideradar/task.rb
+++ lib/glideradar/task.rb
@@ -8,9 +8,25 @@
 
 require 'ygg/agent/task'
 
+
+require 'objspace'
+
 module Glideradar
 
 class Task < Ygg::Agent::Task
+  operation :dump_mem do
+    help_caption ''
+    help_text ''
+  end
+  def dump_mem
+    {
+     count: ObjectSpace.count_objects,
+     size: ObjectSpace.count_objects_size,
+     nodes: ObjectSpace.count_nodes,
+     tdata: ObjectSpace.count_tdata_objects,
+    }
+  end
+
 end
 
 end
